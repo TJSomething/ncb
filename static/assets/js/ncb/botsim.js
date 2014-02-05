23e8273af72fb9013ebbd1d5a4e62c0e2b3bd7f1
Thomas Kelly <tjbk123@gmail.com>
2014-02-05 00:56:57 -0800

    Refactored to use observer pattern

--- static/assets/js/ncb/botsim.js
+++ static/assets/js/ncb/botsim.js
@@ -3,63 +3,141 @@
 
 'use strict';
 
+var BOTSIM = BOTSIM || {
+    // Constants:
+    ASPECT: 16 / 9,
+    FPS: 60
+};
+
+
+// Convienience functions and classes
 // Yay, incompatibility! Probably should put this in its own file
 window.requestAnimationFrame = window.requestAnimationFrame ||
                                window.mozRequestAnimationFrame ||
                                window.webkitRequestAnimationFrame ||
                                window.msRequestAnimationFrame;
 
-var BOTSIM = BOTSIM || {};
+// A nice implementation of the observer pattern from
+//  Javascript patterns by Stoyan Stefanov
+var publisher = {
+    subscribers: {
+        any: []
+    },
+    on: function (type, fn, context) {
+        type = type || 'any';
+        fn = typeof fn === 'function' ? fn : context[fn];
+        
+        if (typeof this.subscribers[type] === 'undefined') {
+            this.subscribers[type] = [];
+        }
+        this.subscribers[type].push({fn: fn, context: context || this});
+    },
+    remove: function (type, fn, context) {
+        this.visitSubscribers('unsubscribe', type, fn, context);
+    },
+    fire: function (type, publication) {
+        this.visitSubscribers('publish', type, publication);
+    },
+    visitSubscribers: function (action, type, arg, context) {
+        var pubtype = type || 'any',
+            subscribers = this.subscribers[pubtype],
+            i,
+            max = subscribers ? subscribers.length : 0;
+            
+        for (i = 0; i < max; i += 1) {
+            if (action === 'publish') {
+                subscribers[i].fn.call(subscribers[i].context, arg);
+            } else {
+                if (subscribers[i].fn === arg && subscribers[i].context === context) {
+                    subscribers.splice(i, 1);
+                }
+            }
+        }
+    }
+};
 
-(function (app) {
+
+function makePublisher(o) {
+    var i;
+    for (i in publisher) {
+        if (publisher.hasOwnProperty(i) && typeof publisher[i] === 'function') {
+            o[i] = publisher[i];
+        }
+    }
+    o.subscribers = {any: []};
+}
+
+makePublisher(BOTSIM);
+
+// The actual game logic
+BOTSIM.initViewport = function () {
     var width, height;
 
-    app.ASPECT = 16 / 9;
+    this.container = document.getElementById('botsim-body');
+    width = this.container.offsetWidth;
+    height = width / this.ASPECT;
 
-    // Initialize the view
-    app.container = document.getElementById('botsim-body');
-    width = app.container.offsetWidth;
-    height = width / app.ASPECT;
+    this.renderer = new THREE.WebGLRenderer();
 
-    app.renderer = new THREE.WebGLRenderer();
+    this.renderer.setSize(width, height);
+    this.container.appendChild(this.renderer.domElement);
+};
 
-    app.renderer.setSize(width,
-        height);
-    app.container.appendChild(app.renderer.domElement);
+BOTSIM.initScene = function () {
+    var that = this;
 
-    app.camera = new THREE.PerspectiveCamera(45,
-            app.ASPECT, 1, 1000);
-    app.camera.position.set(0, 0, 3);
+    this.scene = new THREE.Scene();
 
-    // Initialize the scene
-    app.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera(45,
+            this.ASPECT, 1, 1000);
+    this.camera.position.set(0, 0, 3);
+    this.scene.add(this.camera);
 
-    app.scene.add(app.camera);
+    this.lights = {};
+    this.lights.directed = new THREE.DirectionalLight(0xffffff, 1.0);
+    this.lights.directed.position.set(0, 1, 1);
+    this.scene.add(this.lights.directed);
 
-    app.lights = {};
-    app.lights.directed = new THREE.DirectionalLight(0xffffff, 1.0);
-    app.lights.directed.position.set(0, 1, 1);
-    app.scene.add(app.lights.directed);
+    this.lights.ambient = new THREE.AmbientLight(0x202020);
+    this.scene.add(this.lights.ambient);
 
-    app.lights.ambient = new THREE.AmbientLight(0x202020);
-    app.scene.add(app.lights.ambient);
+    this.loader = new THREE.ObjectLoader();
 
-    app.loader = new THREE.ObjectLoader();
-
-    app.loader.load('/assets/json/mouse.js', function (obj) {
-        app.mouse = obj;
-        app.mouse.scale.set(0.2, 0.2, 0.2);
-        app.scene.add(app.mouse);
+    this.loader.load('/assets/json/mouse.js', function (obj) {
+        that.mouse = obj;
+        that.mouse.scale.set(0.2, 0.2, 0.2);
+        that.scene.add(that.mouse);
+        that.fire('scene-loaded');
     });
+};
+
+BOTSIM.run = function () {
+    var that = this;
 
     // Animate
     function run() {
-        app.renderer.render(app.scene, app.camera);
-        if (app.mouse) {
-            app.mouse.rotation.y -= 0.01;
-        }
+        that.renderer.render(that.scene, that.camera);
 
         window.requestAnimationFrame(run);
+        that.fire('render');
     }
     run();
+
+    // A physics and stuff loop
+    window.setInterval(this.fire.bind(this, 'tick'), 1000/this.FPS);
+};
+
+// When the scene's loaded, run the animation and the physics
+BOTSIM.on('scene-loaded', 'run', BOTSIM);
+
+BOTSIM.on('tick', function () {
+    this.mouse.rotation.y += 0.01;
+}, BOTSIM);
+
+(function (app) {
+    // Initialize the view
+    app.initViewport();
+
+    // Initialize the scene
+    app.initScene();
 }(BOTSIM));
